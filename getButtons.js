299847e80f1ecf9189cc2478535328e610f7b18a
koshmaymm <michail_koshmay@mail.ru>
2017-08-08 13:00:14 +0300

    change js

--- getButtons.js
+++ getButtons.js
@@ -1,8 +1,8 @@
- let example = document.getElementById("canvasTest");
+/* let example = document.getElementById("canvasTest");
  let thickness = document.getElementById("lineWidth");
  let clear = document.getElementById("clearCanvas");
  let clearStorage = document.getElementById("clearStorage");
  let moove = document.getElementById("moove");
  let canvasColor = document.getElementById("canvasColor");
  let addPicture = document.getElementById("addPicture");
- let addData = document.getElementById("addData");
+ let addData = document.getElementById("addData");*/
