ca73eef3c370a20eeade5b724c50d5d7cca4d2e0
koshmaymm <michail_koshmay@mail.ru>
2017-08-08 13:34:31 +0300

    change html

--- getButtons.js
+++
@@ -1,8 +0,0 @@
-/* let example = document.getElementById("canvasTest");
- let thickness = document.getElementById("lineWidth");
- let clear = document.getElementById("clearCanvas");
- let clearStorage = document.getElementById("clearStorage");
- let moove = document.getElementById("moove");
- let canvasColor = document.getElementById("canvasColor");
- let addPicture = document.getElementById("addPicture");
- let addData = document.getElementById("addData");*/
