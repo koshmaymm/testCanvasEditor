28772a4396de836991aead82df49e37d0eda3d75
koshmaymm <michail_koshmay@mail.ru>
2017-08-21 15:26:06 +0300

    add webpack.config.js

---
+++ webpack.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+    entry: './app.js',
+    output: {
+      filename: 'scripts/bundle.js'
+    }
+  };
+
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        loader: 'babel-loader?presets[]=es2015'
+      }
+    ]
+  }
